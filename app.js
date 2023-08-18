ef9fc6eeea96866b48c82689c1d2330a26011527
Kumari Riya <riyachouhan.ee@gmail.com>
2023-08-18 18:34:04 +0530

    f

--- app.js
+++ app.js
@@ -9,11 +9,18 @@ app.use(express.json());
 app.use(cors());
 app.use("/books", router) //localhost:5000/books
 
+const port = process.env.PORT || 5001; // Default to port 5001 if PORT is not set
+
+app.get("/",(req,res)=>{
+  res.send("Hellow world");
+})
+
+
 
 mongoose.connect(
   "mongodb+srv://riya:riya123@bookstall.lgfpgls.mongodb.net/bookstall?retryWrites=true&w=majority"
 )
 .then(()=>console.log("Database Connected!"))
 .then(() => {
-  app.listen(5000);
+  app.listen(port);
 }).catch((err) => console.log(err));
